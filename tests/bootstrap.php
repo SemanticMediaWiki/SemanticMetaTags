ba793de77e966b6895ae550bb7093ccf927fad46
mwjames <jamesin.hongkong.1@gmail.com>
2015-08-04 22:16:36 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,12 @@ if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	print "\nSemanticMediaWiki " . SMW_VERSION . " ({$GLOBALS['wgDBtype']}) test autoloader ...\n";
-} else {
+if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
 	die( 'The SemanticMediaWiki test autoloader is not available' );
 }
 
 $autoloader = require $autoloaderClassPath;
 $autoloader->addPsr4( 'SMT\\Tests\\', __DIR__ . '/phpunit/Unit' );
 $autoloader->addPsr4( 'SMT\\Tests\\Integration\\', __DIR__ . '/phpunit/Integration' );
+
+print( "Semantic MediaWiki: " . SMW_VERSION . " ({$GLOBALS['smwgDefaultStore']}, {$GLOBALS['wgDBtype']})\n\n" );
