c44ad1590562df0075c2e13b50002c20934e3d99
mwjames <jamesin.hongkong.1@gmail.com>
2020-05-10 10:24:43 +0900

    [CI] Set minimum requirements PHPUnit 6.5, MW 1.31, SMW 3.1

--- tests/phpunit/Unit/OptionsTest.php
+++ tests/phpunit/Unit/OptionsTest.php
@@ -43,7 +43,7 @@ class OptionsTest extends \PHPUnit_Framework_TestCase {
 
 		$instance = new Options();
 
-		$this->setExpectedException( 'InvalidArgumentException' );
+		$this->expectException( 'InvalidArgumentException' );
 		$instance->get( 'Foo' );
 	}
 
